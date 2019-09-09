de9f4374a9cbb831abe827a13ab5d425fb81b9b2
Dominik Szmek <do@minik.pl>
2019-09-09 20:55:23 +0200

    add views

--- stations/urls.py
+++ stations/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 
+app_name = 'stations'
 
 urlpatterns = [
     path('', views.RadListView.as_view(), name='index'),
+    path('add/', views.RadCreateView.as_view(), name='create'),
+    path('<int:pk>/update/', views.RadUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.RadDeleteView.as_view(), name='delete'),
+
+    # login
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- stations/views.py
+++ stations/views.py
@@ -1,8 +1,28 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView, CreateView,\
+    DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Radcheck
 
 
-class RadListView(ListView):
+class RadListView(LoginRequiredMixin, ListView):
 
     model = Radcheck
+
+
+class RadCreateView(LoginRequiredMixin, CreateView):
+
+    model = Radcheck
+    fields = ['username', 'attribute', 'value', 'mac', 'description']
+
+
+class RadUpdateView(LoginRequiredMixin, UpdateView):
+
+    model = Radcheck
+    fields = ['username', 'attribute', 'value', 'mac', 'description']
+
+
+class RadDeleteView(LoginRequiredMixin, DeleteView):
+
+    model = Radcheck
+    success_url = '/'
